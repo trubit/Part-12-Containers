ace18201d8dd7d4151ada8cc05283be92ff14ff5
Ezika trust chidi <trustezika831@gmail.com>
2026-07-29 20:44:27 +0100

    Add Redis todo counter, statistics endpoint, Redis persistence volume, and exercise11 log

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require("../util/config");
+const { get } = require("../redis");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
+});
+
+router.get("/statistics", async (_, res) => {
+  let addedTodos = 0;
+
+  try {
+    const count = await get("added_todos");
+    addedTodos = Number(count ?? 0);
+  } catch (error) {
+    console.error("Failed to read added_todos from Redis:", error);
+  }
+
+  res.send({ added_todos: addedTodos });
 });
 
 module.exports = router;
